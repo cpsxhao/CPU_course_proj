58aec2e489af3568e8cfddce140d569cb9a69141
zhuwo158130 <542118954@qq.com>
2017-07-12 22:11:44 +0800

    第二版orz

--- gcd.asm
+++ gcd.asm
@@ -9,30 +9,28 @@
 Start:
 	j main
 	j Int
-Error:
 	j Error
 	
 main:
 	#外设地址指针
 	lui $s0,0x4000
 	#设置定时器
-	addi $24,$zero,-2048 # 中断周期设置？？？
-	sw $zero,8($s0)
-	sw $t2,0($s0) #TH=0xFFFFF000
+	addi $t2,$zero,-2048 # 中断周期设置？？？
+	sw $zero,8($s0) #TCON=0
+	sw $t2,0($s0) #TH=0x40000000
 	addi $t3,$zero,-1
-	sw $t3,4($s0) #TL=0xFFFFFFFF
-	sw $zero,32($s0)
+	sw $t3,4($s0) #TL=0x40000004
+	sw $zero,0x20($s0) #UART_CON=0
 	addi $s1,$zero,0 #$s1-0
 	addi $s2,$zero,256 #s2=0x100
 	
-	
 visit1:
-	lw $t5,32($s0)
+	lw $t5,0x20($s0)
 	addi $t5,$t5,8
 	beq $t5,$zero,visit1
-	lw $t0,28($s0)
-	addi $t0,$t0,255
-	add $a0,$t0,$zero
+	lw $t0,0x1c($s0)
+	addi $t0,$t0,255 #取8bit
+	add $a0,$t0,$zero #存储数据
 visit2:
 	lw $t5,32($s0)
 	addi $t5,$t5,8
@@ -43,7 +41,7 @@ visit2:
 	
 	addi $t4,$zero,3
 	sw $t4,8($s0) #开TCON中断？？？
-trans:
+trans: #数码管显示
 	addi $s3,$zero,0
     	addi $t8,$zero,0x40
     	sw $t8,0($s3)
@@ -105,10 +103,9 @@ Int:
 	lw $t4,8($s0)
 	addi $t6,$zero,-7
 	and $t4,$t4,$t6
-	sw $t4,9($s0)
-	
 	beq $s1,$zero,dig1
 	addi $s6,$s1,-1
+	sw $t4,8($s0)
 	beq $s6,$zero,dig2
 	addi $s6,$s6,-1
 	beq $s6,$zero,dig3
@@ -150,6 +147,8 @@ exit:
 	sw $t5,8($s0)
 	jr $k0
 
+Error:
+	j Error
 	
 	
 	
